0d201233a93f65d25a3b68dab5bcd296216721cf
CorentinPtrl <33176298+CorentinPtrl@users.noreply.github.com>
2021-12-22 16:56:47 +0100

    Added toString Method for VectorUE4

--- SotCoreTest/Program.cs
+++ SotCoreTest/Program.cs
@@ -5,28 +5,22 @@ namespace SotEspCoreTest
 {
     class Program
     {
-
-        public static string convertVecToStr(VectorUE4 vec)
-        {
-            if (vec == null)
-                return "NULL";
-            return "(X: " + vec.getX() + " Y: " + vec.getY() + " Z: " + vec.getZ() + ")";
-        }
         static void Main(string[] args)
         {
             SotCore core = new SotCore();
+            
             if (core.Prepare(false))
             {
                 UE4Actor[] actors = core.GetActors();
                 foreach (UE4Actor actor in actors)
                 {
-                    Console.WriteLine("Name : " + actor.BaseName + " | Actual Name :" + actor.getName() + " Pos :" + convertVecToStr(actor.getPos()));
+                    Console.WriteLine("Name : {0} | Actual Name : {1} Pos : {2} Rot : {3}", actor.BaseName, actor.getName(), actor.getPos(), actor.getRot());
                 }
                 UE4Actor localPlayer = core.GetLocalPlayer();
                 int i = 1;
                 while (true)
                 {
-                    Console.WriteLine("LocalPlayer Name : {0} | LocalPlayer Actual Name : {1} LocalPlayer Pos : {2} index = {3}", localPlayer.BaseName, localPlayer.getName(), convertVecToStr(localPlayer.getPos()), i);
+                    Console.WriteLine("LocalPlayer Name : {0} | LocalPlayer Actual Name : {1} LocalPlayer Pos : {2} LocalPLayer Rot : {3} index = {4}", localPlayer.BaseName, localPlayer.getName(), localPlayer.getPos(),localPlayer.getRot(), i);
                     i++;
                 }
             }
